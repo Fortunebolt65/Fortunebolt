3e7ec45f5d13ac37b686257a63de85d5fddad242
Fortunebolt65 <52884312+Fortunebolt65@users.noreply.github.com>
2020-03-27 07:52:06 +0100

    added first js file

---
+++ first_JS_task.js
@@ -0,0 +1,10 @@
+const fullName = "Onyemuwa Fortune Chibueze";
+const courses = ["Design", "Front-End"];
+
+console.log(fullName)
+console.log(courses[0])
+console.log(courses[1])
+for (let i = courses.length; i <= 200; i++) {
+    if (i % 2 === 0)
+     console.log(i)
+    }
